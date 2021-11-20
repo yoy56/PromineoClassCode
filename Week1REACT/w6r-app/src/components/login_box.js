ef51b6f79ecf16b856d9b9c8a15c255549254e91
Rory B-J <roryboy1102@gmail.com>
2021-11-20 12:37:58 -0600

    Added Top Margin

--- Week1REACT/w6r-app/src/components/login_box.js
+++ Week1REACT/w6r-app/src/components/login_box.js
@@ -3,13 +3,13 @@ import React from "react";
 export default class loginBox extends React.Component {
     render() {
         return (
-            <div className='border border-info rounded-bottom rounded-3 bg-light w-25 mx-auto'>
+            <div className='border border-info rounded-bottom rounded-3 bg-light w-25 mx-auto mt-5'>
                 <h3 className='text-primary text-center'>Log In</h3>
                 <div className='container text-center'>
                     <form>
-                        <input type='text' placeholder='Username'></input> <br/>
-                        <input type='password' placeholder='Password'></input> <br/>
-                        <button type='submit'>Login</button>
+                        <input type='text' placeholder='Username' className='overflow-hidden w-100'></input> <br/>
+                        <input type='password' placeholder='Password' className='overflow-hidden w-100'></input> <br/>
+                        <button type='submit' >Login</button>
                     </form>
                 </div>
             </div>
